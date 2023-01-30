fa10ccfbd200e85fbeb497ad48f4aa2ccf6170cb
amr_amy <amr_amy@hotmail.com>
2023-01-30 10:43:25 -0500

    Part 2 Summary Stats on Suspension

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -1,7 +1,7 @@
 # Import dependency
 library(dplyr)
 
-# Import/ Read csv file
+# Import and Read MechaCar_mpg.csv file
 MechaCar <- read.csv('MechaCar_mpg.csv', stringsAsFactors = F)
 
 # get column names
@@ -12,3 +12,19 @@ colnames(MechaCar)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar)
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCar))
+
+
+# Import and read Suspension_Coil.csv
+Suspension <- read.csv("Suspension_Coil.csv", stringsAsFactors = F)
+
+#get column names
+colnames(Suspension)
+
+# use summarize() function to get mean, median, variance and sd of PSI column.
+
+total_summary <- Suspension %>% summarise(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
+
+#Create dataframe using group_by() and summarise() to group each manufacturing lot by mean, median, variance and SD of suspension coil's PSI column
+lot_summary <- Suspension %>% group_by(Manufacturing_Lot)%>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI),.groups = 'keep')
+
+?summarise()
